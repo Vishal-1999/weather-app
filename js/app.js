9c75f5002e39754f896c302a0c5ae492879669d9
vishal-1999 <vishalsharma161999@gmail.com>
2018-03-26 15:00:22 +0530

    working api : just need to add click converter

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   var userLon = 'lon=';
   var currentTempInCelius = 0;
   var tempUnit = 'C';
+
+
+
   
   // seting latitude and longitude to current position
  if(navigator.geolocation){
@@ -13,15 +16,33 @@ $(document).ready(function(){
   var api = 'https://fcc-weather-api.glitch.me/api/current?' + userLat + '&' + userLon;
   
   $.getJSON(api, function(json){
-    console.log(json);
+    
     currentTempInCelius = json.main.temp;
     $('#temp').text(currentTempInCelius + String.fromCharCode(176));
     $('#tempunit').text(tempUnit);
     $('#city').text(json.name);
     $('#country').text(json.sys.country);
-    //$('#weather').html(json.weather[0].name);
+
     $('#weather').text(json.weather[0].description);
+
+    //farenheight from celius
+
+  function getFTemp(){
+    return Math.ceil((currentTempInCelius * 9/5) + 32);
+  }
+  //unitChanger
+
+  $('#tempunit').on('click', function(){
+    if(tempUnit == C){
+      $('#tempunit').text('F');
+      $('#temp').text(getFTemp());
+    }else{
+      $('#tempunit').text('C');
+      $('#temp').text(currentTempInCelius);
+    }
   });
+  });
+
    });
  }
   
--- js/main.js
+++
@@ -1,4 +0,0 @@
-function f(){
-	document.getElementByTagName("span").innerHTML = 'span';
-}
-
